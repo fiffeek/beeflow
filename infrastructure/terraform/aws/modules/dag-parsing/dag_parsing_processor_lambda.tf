da40bd069b14dbcd264ad106d39429c7d67c6f75
fiffeek <filipmikina@gmail.com>
2022-06-28 23:19:05 +0200

    Add S3 access to DAG processing lambda

--- infrastructure/terraform/aws/modules/dag-parsing/dag_parsing_processor_lambda.tf
+++ infrastructure/terraform/aws/modules/dag-parsing/dag_parsing_processor_lambda.tf
@@ -25,11 +25,42 @@ module "dag_parsing_processor_lambda" {
     additional_environment_variables = {
       BEEFLOW__DAGS_BUCKET_NAME = var.dags_code_bucket.name
     }
-    memory_size                    = 256
+    memory_size                    = 512
     reserved_concurrent_executions = 1
   }
   subnet_ids = var.subnet_ids
   vpc_sg     = var.vpc_sg
 
   context = module.dag_parsing_processor_lambda_label
+}
+
+data "aws_iam_policy_document" "readonly_dags_access" {
+  statement {
+    sid = "ReadonlyDAGsParsingProcessorAccess"
+    actions = [
+      "s3:Get*",
+      "s3:List*"
+    ]
+    resources = [
+      var.dags_code_bucket.arn,
+      "${var.dags_code_bucket.arn}/*"
+    ]
+  }
+}
+
+module "readonly_dags_access_label" {
+  source  = "cloudposse/label/null"
+  version = "0.25.0"
+  name    = "readonly-dags-access"
+  context = module.this
+}
+
+resource "aws_iam_policy" "readonly_dags_access" {
+  name   = module.readonly_dags_access_label.id
+  policy = data.aws_iam_policy_document.readonly_dags_access.json
+}
+
+resource "aws_iam_role_policy_attachment" "readonly_dags_access" {
+  role       = module.dag_parsing_processor_lambda.role_name
+  policy_arn = aws_iam_policy.readonly_dags_access.arn
 }
