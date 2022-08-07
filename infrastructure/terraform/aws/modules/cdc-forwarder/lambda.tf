bc399585aff6a756be4d2d60b003e12d1e7248a2
fiffeek <filipmikina@gmail.com>
2022-08-07 12:01:57 +0100

    Add CDC forwarder lambda terraform creation resources

---
+++ infrastructure/terraform/aws/modules/cdc-forwarder/lambda.tf
@@ -0,0 +1,36 @@
+module "cdc_forwarder_lambda_label" {
+  source  = "cloudposse/label/null"
+  version = "0.25.0"
+  name    = "cdc-forwarder"
+  context = module.this
+}
+
+
+module "cdc_forwarder_lambda" {
+  source = "../airflow-private-lambda"
+
+  airflow_home                             = var.airflow_home
+  appconfig_application_configuration_name = var.appconfig_application_configuration_name
+  appconfig_application_name               = var.appconfig_application_name
+  is_lambda_dockerized                     = false
+  is_lambda_packaged                       = true
+
+  lambda_packaged_spec = {
+    lambda_code_bucket_name     = var.lambda_code_bucket_name
+    package_absolute_path       = var.package_absolute_path
+    package_filename            = var.package_filename
+    pants_lambda_entrypoint     = var.pants_lambda_entrypoint
+    pants_lambda_python_version = var.pants_lambda_python_version
+  }
+
+  spec = {
+    timeout                          = 60
+    additional_environment_variables = {}
+    memory_size                      = 128
+    reserved_concurrent_executions   = 1
+  }
+  subnet_ids = var.subnet_ids
+  vpc_sg     = var.vpc_sg
+
+  context = module.cdc_forwarder_lambda_label
+}
---
+++ infrastructure/terraform/aws/modules/cdc-forwarder/variables.tf
@@ -0,0 +1,49 @@
+variable "lambda_code_bucket_name" {
+  type        = string
+  description = "Name of the lambda code bucket."
+}
+
+variable "vpc_sg" {
+  type        = string
+  description = "The Id of the VPC security group to attach to the lambda."
+}
+
+variable "subnet_ids" {
+  type        = list(string)
+  description = "List of subnet IDs associated with the Lambda function."
+}
+
+variable "pants_lambda_entrypoint" {
+  type        = string
+  description = "Lambda entrypoint for pants generated packaged."
+}
+
+variable "pants_lambda_python_version" {
+  type        = string
+  description = "The version of python that Pants built the package with."
+}
+
+variable "appconfig_application_name" {
+  type        = string
+  description = "The name of the AppConfig Application."
+}
+
+variable "appconfig_application_configuration_name" {
+  type        = string
+  description = "The name of the AppConfig Application Configuration."
+}
+
+variable "airflow_home" {
+  type        = string
+  description = "Airflow home directory."
+}
+
+variable "package_absolute_path" {
+  type        = string
+  description = "Absolute path to the CDC input forwarder lambda package."
+}
+
+variable "package_filename" {
+  type        = string
+  description = "Filename of the CDC input forwarder lambda package."
+}
