e432fd3fa33cd9f4c0da90f409ba0c0b1f8d784f
fiffeek <filipmikina@gmail.com>
2022-09-19 17:41:19 +0100

    Add scheduler terraform outputs

---
+++ infrastructure/terraform/aws/modules/scheduler/outputs.tf
@@ -0,0 +1,9 @@
+output "scheduler_sqs_arn" {
+  value = aws_sqs_queue.scheduler_sqs.arn
+  description = "The ARN of the SQS queue feeding the scheduler"
+}
+
+output "scheduler_sqs_id" {
+  value = aws_sqs_queue.scheduler_sqs.id
+  description = "The id of the SQS queue feeding the scheduler"
+}
