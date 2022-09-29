5cd7dae38720cc0854c5da0a15c5312b3f89bfae
fiffeek <filipmikina@gmail.com>
2022-09-29 20:30:06 +0200

    Remove task adoption on startup in the scheduler

--- src/python/beeflow/lambdas/scheduler/scheduler_job.py
+++ src/python/beeflow/lambdas/scheduler/scheduler_job.py
@@ -752,12 +752,6 @@ class SchedulerJob(BaseJob):
         timers = EventScheduler()
 
         # Check on start up, then every configured interval
-        self.adopt_or_reset_orphaned_tasks()
-
-        timers.call_regular_interval(
-            conf.getfloat('scheduler', 'orphaned_tasks_check_interval', fallback=300.0),
-            self.adopt_or_reset_orphaned_tasks,
-        )
 
         timers.call_regular_interval(
             conf.getfloat('scheduler', 'trigger_timeout_check_interval', fallback=15.0),
@@ -1149,94 +1143,6 @@ class SchedulerJob(BaseJob):
     @provide_session
     def heartbeat_callback(self, session: Session = None) -> None:
         Stats.incr('scheduler_heartbeat', 1, 1)
-
-    @provide_session
-    def adopt_or_reset_orphaned_tasks(self, session: Session = None):
-        """
-        Reset any TaskInstance still in QUEUED or SCHEDULED states that were
-        enqueued by a SchedulerJob that is no longer running.
-
-        :return: the number of TIs reset
-        :rtype: int
-        """
-        self.log.info("Resetting orphaned tasks for active dag runs")
-        timeout = conf.getint('scheduler', 'scheduler_health_check_threshold')
-
-        for attempt in run_with_db_retries(logger=self.log):
-            with attempt:
-                self.log.debug(
-                    "Running SchedulerJob.adopt_or_reset_orphaned_tasks with retries. Try %d of %d",
-                    attempt.retry_state.attempt_number,
-                    MAX_DB_RETRIES,
-                )
-                self.log.debug("Calling SchedulerJob.adopt_or_reset_orphaned_tasks method")
-                try:
-                    num_failed = (
-                        session.query(SchedulerJob)
-                        .filter(
-                            SchedulerJob.job_type == "SchedulerJob",
-                            SchedulerJob.state == State.RUNNING,
-                            SchedulerJob.latest_heartbeat < (timezone.utcnow() - timedelta(seconds=timeout)),
-                        )
-                        .update({"state": State.FAILED})
-                    )
-
-                    if num_failed:
-                        self.log.info("Marked %d SchedulerJob instances as failed", num_failed)
-                        Stats.incr(self.__class__.__name__.lower() + '_end', num_failed)
-
-                    resettable_states = [TaskInstanceState.QUEUED, TaskInstanceState.RUNNING]
-                    query = (
-                        session.query(TI)
-                        .filter(TI.state.in_(resettable_states))
-                        # outerjoin is because we didn't use to have queued_by_job
-                        # set, so we need to pick up anything pre upgrade. This (and the
-                        # "or queued_by_job_id IS NONE") can go as soon as scheduler HA is
-                        # released.
-                        .outerjoin(TI.queued_by_job)
-                        .filter(or_(TI.queued_by_job_id.is_(None), SchedulerJob.state != State.RUNNING))
-                        .join(TI.dag_run)
-                        .filter(
-                            DagRun.run_type != DagRunType.BACKFILL_JOB,
-                            DagRun.state == State.RUNNING,
-                        )
-                        .options(load_only(TI.dag_id, TI.task_id, TI.run_id))
-                    )
-
-                    # Lock these rows, so that another scheduler can't try and adopt these too
-                    tis_to_reset_or_adopt = with_row_locks(
-                        query, of=TI, session=session, **skip_locked(session=session)
-                    ).all()
-                    to_reset = self.executor.try_adopt_task_instances(tis_to_reset_or_adopt)
-
-                    reset_tis_message = []
-                    for ti in to_reset:
-                        reset_tis_message.append(repr(ti))
-                        ti.state = State.NONE
-                        ti.queued_by_job_id = None
-
-                    for ti in set(tis_to_reset_or_adopt) - set(to_reset):
-                        ti.queued_by_job_id = self.id
-
-                    Stats.incr('scheduler.orphaned_tasks.cleared', len(to_reset))
-                    Stats.incr('scheduler.orphaned_tasks.adopted', len(tis_to_reset_or_adopt) - len(to_reset))
-
-                    if to_reset:
-                        task_instance_str = '\n\t'.join(reset_tis_message)
-                        self.log.info(
-                            "Reset the following %s orphaned TaskInstances:\n\t%s",
-                            len(to_reset),
-                            task_instance_str,
-                        )
-
-                    # Issue SQL/finish "Unit of Work", but let @provide_session
-                    # commit (or if passed a session, let caller decide when to commit
-                    session.flush()
-                except OperationalError:
-                    session.rollback()
-                    raise
-
-        return len(to_reset)
 
     @provide_session
     def check_trigger_timeouts(self, session: Session = None):
