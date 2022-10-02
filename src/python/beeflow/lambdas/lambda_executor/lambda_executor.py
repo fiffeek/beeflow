03cb2199a333094091e29e2ddb8b3851bd592ccc
fiffeek <filipmikina@gmail.com>
2022-10-02 21:07:29 +0200

    Replace local task job with local executor for the lambda worker

--- src/python/beeflow/lambdas/lambda_executor/lambda_executor.py
+++ src/python/beeflow/lambdas/lambda_executor/lambda_executor.py
@@ -3,7 +3,8 @@ from typing import Dict, Any, List, Optional
 
 from airflow import DAG, AirflowException
 from airflow.cli.commands.task_command import _get_ti, _capture_task_logs
-from airflow.jobs.local_task_job import LocalTaskJob
+from airflow.executors.executor_constants import LOCAL_EXECUTOR
+from airflow.executors.executor_loader import ExecutorLoader
 from airflow.utils.cli import process_subdir
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities.parser import parse, envelopes, event_parser
@@ -19,16 +20,17 @@ logger = Logger()
 # TODO: Rewrite this executor to use Step functions so if any issue arises there is a watcher
 # that propagates the issues to the database.
 
-def _run_task_by_local_task_job(pool, task_instance):
-    """Run LocalTaskJob, which monitors the raw task execution process"""
-    run_job = LocalTaskJob(
-        task_instance=task_instance,
+def _run_task_by_executor(pool, task_instance):
+    """Sends the task to the executor for execution."""
+    executor = ExecutorLoader.load_executor(executor_name=ExecutorLoader.executors[LOCAL_EXECUTOR])
+    executor.job_id = "manual"
+    executor.start()
+    executor.queue_task_instance(
+        task_instance,
         pool=pool,
     )
-    try:
-        run_job.run()
-    finally:
-        logging.shutdown()
+    executor.heartbeat()
+    executor.end()
 
 
 def get_dag(subdir: Optional[str], dag_id: str) -> "DAG":
@@ -43,8 +45,6 @@ def get_dag(subdir: Optional[str], dag_id: str) -> "DAG":
     return dagbag.dags[dag_id]
 
 
-# Change to https://github.com/apache/airflow/blob/ce071172e22fba018889db7dcfac4a4d0fc41cda/airflow/cli/commands/task_command.py#L196 to
-# execute via LocalExecutor instead
 def execute_work(event: TaskInstanceQueued):
     # Assumes the task code lives under DAGS_FOLDER
     dag: DAG = get_dag("DAGS_FOLDER", event.dag_id)
@@ -52,7 +52,7 @@ def execute_work(event: TaskInstanceQueued):
     ti, _ = _get_ti(task, exec_date_or_run_id=event.run_id, map_index=event.map_index, pool=event.pool)
     ti.init_run_context()
     with _capture_task_logs(ti):
-        _run_task_by_local_task_job(pool=event.pool, task_instance=ti)
+        _run_task_by_executor(pool=event.pool, task_instance=ti)
 
 
 @logger.inject_lambda_context
