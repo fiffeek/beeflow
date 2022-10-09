105f58e453ebcbf102b922067e8b2fbd968bb4e1
fiffeek <filipmikina@gmail.com>
2022-10-09 20:04:01 +0200

    Add missing IAM for eventbridhe to the schedule updater SQS

--- infrastructure/terraform/aws/modules/eventbridge/iam.tf
+++ infrastructure/terraform/aws/modules/eventbridge/iam.tf
@@ -5,7 +5,7 @@ resource "aws_sqs_queue_policy" "scheduler" {
 
 data "aws_iam_policy_document" "scheduler" {
   statement {
-    sid     = "events-policy"
+    sid     = "scheduler-events-policy"
     actions = ["sqs:SendMessage"]
 
     principals {
@@ -26,7 +26,7 @@ resource "aws_sqs_queue_policy" "lambda_executor" {
 
 data "aws_iam_policy_document" "lambda_executor" {
   statement {
-    sid     = "events-policy"
+    sid     = "lambda_executor-events-policy"
     actions = ["sqs:SendMessage"]
 
     principals {
@@ -36,6 +36,27 @@ data "aws_iam_policy_document" "lambda_executor" {
 
     resources = [
       var.lambda_executor_sqs.arn
+    ]
+  }
+}
+
+resource "aws_sqs_queue_policy" "dag_schedule_updater" {
+  queue_url = var.dag_schedule_updater_sqs.id
+  policy    = data.aws_iam_policy_document.dag_schedule_updater.json
+}
+
+data "aws_iam_policy_document" "dag_schedule_updater" {
+  statement {
+    sid     = "dag_schedule_updater-events-policy"
+    actions = ["sqs:SendMessage"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+
+    resources = [
+      var.dag_schedule_updater_sqs.arn
     ]
   }
 }
