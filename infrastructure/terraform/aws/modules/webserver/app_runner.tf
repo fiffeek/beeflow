238c3cda5c61d309888563c31d3b0f958cd4aea9
fiffeek <filipmikina@gmail.com>
2022-10-16 20:02:16 +0200

    Update App Runner frontend deployment

--- infrastructure/terraform/aws/modules/webserver/app_runner.tf
+++ infrastructure/terraform/aws/modules/webserver/app_runner.tf
@@ -14,6 +14,22 @@ resource "aws_apprunner_auto_scaling_configuration_version" "webserver" {
   tags = module.this.tags
 }
 
+locals {
+  runtime_args_base = {
+    BEEFLOW__APP_CONFIG_NAME = var.appconfig_application_configuration_name,
+    BEEFLOW__APPLICATION = var.appconfig_application_name,
+    POWERTOOLS_SERVICE_NAME = module.this.id,
+    POWERTOOLS_LOGGER_LOG_EVENT = "true"
+    AIRFLOW_HOME = var.airflow_home,
+    AIRFLOW_CONN_AWS_DEFAULT = "aws://"
+    BEEFLOW__ENVIRONMENT = module.this.environment,
+    PYTHONUNBUFFERED = "1",
+    AIRFLOW__WEBSERVER__UPDATE_FAB_PERMS = "false",
+    # https://stackoverflow.com/a/52451737
+    FORWARDED_ALLOW_IPS = "*",
+  }
+}
+
 resource "aws_apprunner_service" "webserver" {
   service_name = module.this.id
 
@@ -25,17 +41,7 @@ resource "aws_apprunner_service" "webserver" {
     image_repository {
       image_configuration {
         port = var.port
-        runtime_environment_variables = {
-          BEEFLOW__APP_CONFIG_NAME = var.appconfig_application_configuration_name,
-          BEEFLOW__APPLICATION = var.appconfig_application_name,
-          POWERTOOLS_SERVICE_NAME = module.this.id,
-          POWERTOOLS_LOGGER_LOG_EVENT = "true"
-          AIRFLOW_HOME = var.airflow_home,
-          AIRFLOW_CONN_AWS_DEFAULT = "aws://"
-          BEEFLOW__ENVIRONMENT = module.this.environment,
-          PYTHONUNBUFFERED = "1",
-          AIRFLOW__WEBSERVER__UPDATE_FAB_PERMS = "false",
-        }
+        runtime_environment_variables = local.runtime_args_base
       }
       image_identifier = "${var.repository_url}:${var.image_tag}"
       image_repository_type = "ECR"
@@ -56,7 +62,7 @@ resource "aws_apprunner_service" "webserver" {
     interval = 20
     path = "/health"
     protocol = "HTTP"
-    unhealthy_threshold = 20
+    unhealthy_threshold = 10
   }
 
   instance_configuration {
@@ -65,4 +71,3 @@ resource "aws_apprunner_service" "webserver" {
 
   tags = module.this.tags
 }
-
