42d816c5a8ca528447adef8e9155e022a4988301
fiffeek <filipmikina@gmail.com>
2022-10-16 22:38:33 +0200

    Remove unused S3 bucket for logs

--- infrastructure/terraform/aws/modules/buckets/outputs.tf
+++ infrastructure/terraform/aws/modules/buckets/outputs.tf
@@ -17,23 +17,3 @@ output "dags_code_bucket_id" {
   value       = aws_s3_bucket.dags_storage.id
   description = "The ID of a bucket storing DAGs code."
 }
-
-output "airflow_logs_bucket_name" {
-  value       = aws_s3_bucket.airflow_logs.bucket
-  description = "The name of a bucket storing airflow logs."
-}
-
-output "airflow_logs_bucket_arn" {
-  value       = aws_s3_bucket.airflow_logs.arn
-  description = "The name of a bucket storing airflow logs."
-}
-
-output "airflow_logs_bucket_id" {
-  value       = aws_s3_bucket.airflow_logs.id
-  description = "The name of a bucket storing airflow logs."
-}
-
-output "airflow_logs_bucket_key" {
-  value       = local.folder_name
-  description = "The S3 key for airflow logs."
-}
