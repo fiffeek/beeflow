289c4a9c64007e94f6c73eb1b9e559f2e03b6aad
fiffeek <filipmikina@gmail.com>
2022-10-30 20:38:40 +0100

    Increase Airflow webserver startup timeout to 5mins

--- src/python/beeflow/services/webserver/webserver.py
+++ src/python/beeflow/services/webserver/webserver.py
@@ -3,5 +3,5 @@ from airflow.cli.cli_parser import get_parser
 from beeflow.services.webserver.webserver_command import webserver
 
 parser = get_parser()
-empty_args = parser.parse_args(['webserver'])
+empty_args = parser.parse_args(['webserver', '--worker-timeout', '300'])
 webserver(empty_args)
