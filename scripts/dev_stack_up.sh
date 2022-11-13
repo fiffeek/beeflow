045c86766d0a5ae34d5fc93152e717f594b00181
fiffeek <filipmikina@gmail.com>
2022-11-13 18:05:40 +0100

    Fix docker registry arguments from env variables in pantsbuild config file for single source of truth deployment

--- scripts/dev_stack_up.sh
+++ scripts/dev_stack_up.sh
@@ -1,4 +1,8 @@
-export $(cat ../.dev_env | xargs)
+#!/bin/bash
+
+set -e
+
+export "$(cat ../.dev_env | xargs)"
 
 echo "Applying prerequisites"
 cd infrastructure/terragrunt || exit 1
@@ -7,15 +11,20 @@ make apply_ecr-repositories
 cd ../../scripts || exit 1
 
 echo "Logging to ECR"
-aws ecr get-login-password --region "${BEEFLOW_AWS_REGION}" | \
- docker login --username AWS --password-stdin "${BEEFLOW_AWS_ACCOUNT_ID}.dkr.ecr.${BEEFLOW_AWS_REGION}.amazonaws.com"
+aws ecr get-login-password --region "${BEEFLOW_AWS_REGION}" |
+	docker login --username AWS --password-stdin "${BEEFLOW_AWS_ACCOUNT_ID}.dkr.ecr.${BEEFLOW_AWS_REGION}.amazonaws.com"
 
 echo "Publishing artifacts"
 cd .. || exit 1
 ./pants package ::
-./pants export ::
+./pants publish ::
 cd scripts || exit 1
 
 echo "Deploying infrastructure"
 cd infrastructure/terragrunt || exit 1
 make apply_all
+
+echo "Invoking migrations"
+aws lambda invoke \
+	--function-name beeflow-dev-migrations-runner \
+	--payload '{}'
