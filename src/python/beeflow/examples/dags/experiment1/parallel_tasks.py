86babf9c8888a55c9449031a7a931cd4f2679297
fiffeek <filipmikina@gmail.com>
2022-11-22 23:42:02 +0100

    Add a dag for the first experiment with fanout

---
+++ src/python/beeflow/examples/dags/experiment1/parallel_tasks.py
@@ -0,0 +1,30 @@
+import datetime
+
+import pendulum
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+LAMBDA_QUEUE = "lambda"
+
+with DAG(
+        dag_id='beeflow_parallel_tasks',
+        schedule_interval='*/5 * * * *',
+        start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+        catchup=False,
+        dagrun_timeout=datetime.timedelta(minutes=60),
+) as dag:
+    # [START howto_operator_bash]
+    run_this = BashOperator(
+        task_id='run_after_loop',
+        bash_command='echo 1',
+        queue=LAMBDA_QUEUE,
+    )
+    # [END howto_operator_bash]
+
+    for i in range(15):
+        task = BashOperator(
+            task_id='runme_' + str(i),
+            bash_command='echo "{{ task_instance_key_str }}" && sleep 120',
+            queue=LAMBDA_QUEUE,
+        )
+        task >> run_this
