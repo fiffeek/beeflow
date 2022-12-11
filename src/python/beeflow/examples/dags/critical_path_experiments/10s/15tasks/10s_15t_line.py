7d78a14407439890dba42aa580ae3106ecceb17c
fiffeek <filipmikina@gmail.com>
2022-12-11 16:45:38 +0100

    Add critical path experiments (linearized tasks) for 10s task span

---
+++ src/python/beeflow/examples/dags/critical_path_experiments/10s/15tasks/10s_15t_line.py
@@ -0,0 +1,25 @@
+import pendulum
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+TASKS = 15
+
+with DAG(
+    dag_id=f'10s_{TASKS}t_line',
+    schedule_interval='*/5 * * * *',
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+) as dag:
+    tasks = [
+        BashOperator(
+            task_id='runme_' + str(i),
+            bash_command='echo "{{ task_instance_key_str }}" && sleep 10',
+            dag=dag,
+        )
+        for i in range(TASKS)
+    ]
+
+    for i in range(TASKS):
+        if i == 0:
+            continue
+        tasks[i - 1] >> tasks[i]
