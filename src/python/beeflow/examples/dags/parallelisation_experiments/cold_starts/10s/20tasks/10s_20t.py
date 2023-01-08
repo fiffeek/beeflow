3105061ecd12f2aec5fbf6c0b726624328642f96
fiffeek <filipmikina@gmail.com>
2023-01-08 15:25:22 +0100

    Add experiment for DAGs executed on 30m cron to analyse cold starts for MWAA and beeflow

---
+++ src/python/beeflow/examples/dags/parallelisation_experiments/cold_starts/10s/20tasks/10s_20t.py
@@ -0,0 +1,24 @@
+import pendulum
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+TASKS = 20
+
+with DAG(
+    dag_id='10s_20t_30cron',
+    schedule_interval='*/30 * * * *',
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    max_active_runs=1,
+) as dag:
+    run_before_loop = BashOperator(
+        task_id='run_before_loop',
+        bash_command='echo 1',
+    )
+
+    for i in range(TASKS):
+        task = BashOperator(
+            task_id='runme_' + str(i),
+            bash_command='echo "{{ task_instance_key_str }}" && sleep 10',
+        )
+        run_before_loop >> task
