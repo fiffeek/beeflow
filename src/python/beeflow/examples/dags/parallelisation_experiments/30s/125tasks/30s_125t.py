6bb991cca4feb38add1ebd05c36072c73c4dcdaa
fiffeek <filipmikina@gmail.com>
2023-01-17 00:22:25 +0100

    Add experiments definition for 16/32/64/125 tasks in parallel for 10s/30s tasks

---
+++ src/python/beeflow/examples/dags/parallelisation_experiments/30s/125tasks/30s_125t.py
@@ -0,0 +1,23 @@
+import pendulum
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+TASKS = 125
+
+with DAG(
+    dag_id='30s_125t',
+    schedule_interval='*/5 * * * *',
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+) as dag:
+    run_before_loop = BashOperator(
+        task_id='run_before_loop',
+        bash_command='echo 1',
+    )
+
+    for i in range(TASKS):
+        task = BashOperator(
+            task_id='runme_' + str(i),
+            bash_command='echo "{{ task_instance_key_str }}" && sleep 30',
+        )
+        run_before_loop >> task
